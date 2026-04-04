8cd2d94f12e2ca8fa7870af6adb6f8ef6acd0f6c
jeffkashama21-arch <jeffkashama21@gmail.com>
2026-04-04 04:16:12 +0200

    Update question30_lambda.py

--- àws/question30_lambda.py
+++ àws/question30_lambda.py
@@ -1,1 +1,10 @@
+def lambda_handler(event, context):
+  
+    nom = event.get("nom", "utilisateur")
 
+    message = f"Bonjour {nom}, bienvenue dans AWS Lambda"
+
+    return {
+        "statusCode": 200,
+        "body": message
+    }
